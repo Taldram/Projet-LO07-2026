946bb1b8c1f11c50daa09ca3d3f316d1d1f0b0dc
Taldram <c.baudy44@gmail.com>
2026-06-02 13:57:46 +0200

    Virer "cave"

--- src/app/view/viewAccueil.php
+++ src/app/view/viewAccueil.php
@@ -1,20 +1,20 @@
  
-<!-- ----- debut de la page cave_acceuil -->
-<?php include 'fragment/fragmentCaveHeader.html'; ?>
+<!-- ----- debut de la page acceuil -->
+<?php include 'fragment/fragmentHeader.html'; ?>
 <body>
   <div class="container">
     <?php
-    include 'fragment/fragmentCaveMenu.html';
-    include 'fragment/fragmentCaveJumbotron.html';
+    include 'fragment/fragmentMenu.html';
+    include 'fragment/fragmentJumbotron.html';
     ?>
   </div>   
   
   
   <?php
-  include 'fragment/fragmentCaveFooter.html';
+  include 'fragment/fragmentFooter.html';
   ?>
 
-  <!-- ----- fin de la page cave_acceuil -->
+  <!-- ----- fin de la page acceuil -->
 
 </body>
 </html>
